d4ba013a649c5dd991655bdc9f773b9966207446
sjyanxin <sjyanxin@qq.com>
2014-04-29 20:22:32 +0800

    create2

---
+++ DAL/Teacher.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Data;
+using System.Text;
+using System.Data.SqlClient;
+using Maticsoft.DBUtility;//Please add references
+namespace JiaJiao.DAL
+{
+	/// <summary>
+	/// 数据访问类:Teacher
+	/// </summary>
+	public partial class Teacher
+	{
+		public Teacher()
+		{}
+		#region  BasicMethod
+
+		/// <summary>
+		/// 得到最大ID
+		/// </summary>
+		public int GetMaxId()
+		{
+		return DbHelperSQL.GetMaxID("ID", "Teacher"); 
+		}
+
+		/// <summary>
+		/// 是否存在该记录
+		/// </summary>
+		public bool Exists(int ID)
+		{
+			StringBuilder strSql=new StringBuilder();
+			strSql.Append("select count(1) from Teacher");
+			strSql.Append(" where ID=@ID");
+			SqlParameter[] parameters = {
+					new SqlParameter("@ID", SqlDbType.Int,4)
+			};
+			parameters[0].Value = ID;
+
+			return DbHelperSQL.Exists(strSql.ToString(),parameters);
+		}
+
+
+		/// <summary>
+		/// 增加一条数据
+		/// </summary>
+		public int Add(JiaJiao.Model.Teacher model)
+		{
+			StringBuilder strSql=new StringBuilder();
+			strSql.Append("insert into Teacher(");
+			strSql.Append("TeacherName,TeacherTel,TeacherEmail,TeacherAddress,TeacherDescribe,RoleId,CreateTime,UpdateTime)");
+			strSql.Append(" values (");
+			strSql.Append("@TeacherName,@TeacherTel,@TeacherEmail,@TeacherAddress,@TeacherDescribe,@RoleId,@CreateTime,@UpdateTime)");
+			strSql.Append(";select @@IDENTITY");
+			SqlParameter[] parameters = {
+					new SqlParameter("@TeacherName", SqlDbType.NVarChar,50),
+					new SqlParameter("@TeacherTel", SqlDbType.NVarChar,50),
+					new SqlParameter("@TeacherEmail", SqlDbType.NVarChar,50),
+					new SqlParameter("@TeacherAddress", SqlDbType.NVarChar,50),
+					new SqlParameter("@TeacherDescribe", SqlDbType.NVarChar,50),
+					new SqlParameter("@RoleId", SqlDbType.Int,4),
+					new SqlParameter("@CreateTime", SqlDbType.DateTime),
+					new SqlParameter("@UpdateTime", SqlDbType.DateTime)};
+			parameters[0].Value = model.TeacherName;
+			parameters[1].Value = model.TeacherTel;
+			parameters[2].Value = model.TeacherEmail;
+			parameters[3].Value = model.TeacherAddress;
+			parameters[4].Value = model.TeacherDescribe;
+			parameters[5].Value = model.RoleId;
+			parameters[6].Value = model.CreateTime;
+			parameters[7].Value = model.UpdateTime;
+
+			object obj = DbHelperSQL.GetSingle(strSql.ToString(),parameters);
+			if (obj == null)
+			{
+				return 0;
+			}
+			else
+			{
+				return Convert.ToInt32(obj);
+			}
+		}
+		/// <summary>
+		/// 更新一条数据
+		/// </summary>
+		public bool Update(JiaJiao.Model.Teacher model)
+		{
+			StringBuilder strSql=new StringBuilder();
+			strSql.Append("update Teacher set ");
+			strSql.Append("TeacherName=@TeacherName,");
+			strSql.Append("TeacherTel=@TeacherTel,");
+			strSql.Append("TeacherEmail=@TeacherEmail,");
+			strSql.Append("TeacherAddress=@TeacherAddress,");
+			strSql.Append("TeacherDescribe=@TeacherDescribe,");
+			strSql.Append("RoleId=@RoleId,");
+			strSql.Append("CreateTime=@CreateTime,");
+			strSql.Append("UpdateTime=@UpdateTime");
+			strSql.Append(" where ID=@ID");
+			SqlParameter[] parameters = {
+					new SqlParameter("@TeacherName", SqlDbType.NVarChar,50),
+					new SqlParameter("@TeacherTel", SqlDbType.NVarChar,50),
+					new SqlParameter("@TeacherEmail", SqlDbType.NVarChar,50),
+					new SqlParameter("@TeacherAddress", SqlDbType.NVarChar,50),
+					new SqlParameter("@TeacherDescribe", SqlDbType.NVarChar,50),
+					new SqlParameter("@RoleId", SqlDbType.Int,4),
+					new SqlParameter("@CreateTime", SqlDbType.DateTime),
+					new SqlParameter("@UpdateTime", SqlDbType.DateTime),
+					new SqlParameter("@ID", SqlDbType.Int,4)};
+			parameters[0].Value = model.TeacherName;
+			parameters[1].Value = model.TeacherTel;
+			parameters[2].Value = model.TeacherEmail;
+			parameters[3].Value = model.TeacherAddress;
+			parameters[4].Value = model.TeacherDescribe;
+			parameters[5].Value = model.RoleId;
+			parameters[6].Value = model.CreateTime;
+			parameters[7].Value = model.UpdateTime;
+			parameters[8].Value = model.ID;
+
+			int rows=DbHelperSQL.ExecuteSql(strSql.ToString(),parameters);
+			if (rows > 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		/// <summary>
+		/// 删除一条数据
+		/// </summary>
+		public bool Delete(int ID)
+		{
+			
+			StringBuilder strSql=new StringBuilder();
+			strSql.Append("delete from Teacher ");
+			strSql.Append(" where ID=@ID");
+			SqlParameter[] parameters = {
+					new SqlParameter("@ID", SqlDbType.Int,4)
+			};
+			parameters[0].Value = ID;
+
+			int rows=DbHelperSQL.ExecuteSql(strSql.ToString(),parameters);
+			if (rows > 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		/// <summary>
+		/// 批量删除数据
+		/// </summary>
+		public bool DeleteList(string IDlist )
+		{
+			StringBuilder strSql=new StringBuilder();
+			strSql.Append("delete from Teacher ");
+			strSql.Append(" where ID in ("+IDlist + ")  ");
+			int rows=DbHelperSQL.ExecuteSql(strSql.ToString());
+			if (rows > 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+
+		/// <summary>
+		/// 得到一个对象实体
+		/// </summary>
+		public JiaJiao.Model.Teacher GetModel(int ID)
+		{
+			
+			StringBuilder strSql=new StringBuilder();
+			strSql.Append("select  top 1 ID,TeacherName,TeacherTel,TeacherEmail,TeacherAddress,TeacherDescribe,RoleId,CreateTime,UpdateTime from Teacher ");
+			strSql.Append(" where ID=@ID");
+			SqlParameter[] parameters = {
+					new SqlParameter("@ID", SqlDbType.Int,4)
+			};
+			parameters[0].Value = ID;
+
+			JiaJiao.Model.Teacher model=new JiaJiao.Model.Teacher();
+			DataSet ds=DbHelperSQL.Query(strSql.ToString(),parameters);
+			if(ds.Tables[0].Rows.Count>0)
+			{
+				return DataRowToModel(ds.Tables[0].Rows[0]);
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+
+		/// <summary>
+		/// 得到一个对象实体
+		/// </summary>
+		public JiaJiao.Model.Teacher DataRowToModel(DataRow row)
+		{
+			JiaJiao.Model.Teacher model=new JiaJiao.Model.Teacher();
+			if (row != null)
+			{
+				if(row["ID"]!=null && row["ID"].ToString()!="")
+				{
+					model.ID=int.Parse(row["ID"].ToString());
+				}
+				if(row["TeacherName"]!=null)
+				{
+					model.TeacherName=row["TeacherName"].ToString();
+				}
+				if(row["TeacherTel"]!=null)
+				{
+					model.TeacherTel=row["TeacherTel"].ToString();
+				}
+				if(row["TeacherEmail"]!=null)
+				{
+					model.TeacherEmail=row["TeacherEmail"].ToString();
+				}
+				if(row["TeacherAddress"]!=null)
+				{
+					model.TeacherAddress=row["TeacherAddress"].ToString();
+				}
+				if(row["TeacherDescribe"]!=null)
+				{
+					model.TeacherDescribe=row["TeacherDescribe"].ToString();
+				}
+				if(row["RoleId"]!=null && row["RoleId"].ToString()!="")
+				{
+					model.RoleId=int.Parse(row["RoleId"].ToString());
+				}
+				if(row["CreateTime"]!=null && row["CreateTime"].ToString()!="")
+				{
+					model.CreateTime=DateTime.Parse(row["CreateTime"].ToString());
+				}
+				if(row["UpdateTime"]!=null && row["UpdateTime"].ToString()!="")
+				{
+					model.UpdateTime=DateTime.Parse(row["UpdateTime"].ToString());
+				}
+			}
+			return model;
+		}
+
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public DataSet GetList(string strWhere)
+		{
+			StringBuilder strSql=new StringBuilder();
+			strSql.Append("select ID,TeacherName,TeacherTel,TeacherEmail,TeacherAddress,TeacherDescribe,RoleId,CreateTime,UpdateTime ");
+			strSql.Append(" FROM Teacher ");
+			if(strWhere.Trim()!="")
+			{
+				strSql.Append(" where "+strWhere);
+			}
+			return DbHelperSQL.Query(strSql.ToString());
+		}
+
+		/// <summary>
+		/// 获得前几行数据
+		/// </summary>
+		public DataSet GetList(int Top,string strWhere,string filedOrder)
+		{
+			StringBuilder strSql=new StringBuilder();
+			strSql.Append("select ");
+			if(Top>0)
+			{
+				strSql.Append(" top "+Top.ToString());
+			}
+			strSql.Append(" ID,TeacherName,TeacherTel,TeacherEmail,TeacherAddress,TeacherDescribe,RoleId,CreateTime,UpdateTime ");
+			strSql.Append(" FROM Teacher ");
+			if(strWhere.Trim()!="")
+			{
+				strSql.Append(" where "+strWhere);
+			}
+			strSql.Append(" order by " + filedOrder);
+			return DbHelperSQL.Query(strSql.ToString());
+		}
+
+		/// <summary>
+		/// 获取记录总数
+		/// </summary>
+		public int GetRecordCount(string strWhere)
+		{
+			StringBuilder strSql=new StringBuilder();
+			strSql.Append("select count(1) FROM Teacher ");
+			if(strWhere.Trim()!="")
+			{
+				strSql.Append(" where "+strWhere);
+			}
+			object obj = DbHelperSQL.GetSingle(strSql.ToString());
+			if (obj == null)
+			{
+				return 0;
+			}
+			else
+			{
+				return Convert.ToInt32(obj);
+			}
+		}
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		public DataSet GetListByPage(string strWhere, string orderby, int startIndex, int endIndex)
+		{
+			StringBuilder strSql=new StringBuilder();
+			strSql.Append("SELECT * FROM ( ");
+			strSql.Append(" SELECT ROW_NUMBER() OVER (");
+			if (!string.IsNullOrEmpty(orderby.Trim()))
+			{
+				strSql.Append("order by T." + orderby );
+			}
+			else
+			{
+				strSql.Append("order by T.ID desc");
+			}
+			strSql.Append(")AS Row, T.*  from Teacher T ");
+			if (!string.IsNullOrEmpty(strWhere.Trim()))
+			{
+				strSql.Append(" WHERE " + strWhere);
+			}
+			strSql.Append(" ) TT");
+			strSql.AppendFormat(" WHERE TT.Row between {0} and {1}", startIndex, endIndex);
+			return DbHelperSQL.Query(strSql.ToString());
+		}
+
+		/*
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		public DataSet GetList(int PageSize,int PageIndex,string strWhere)
+		{
+			SqlParameter[] parameters = {
+					new SqlParameter("@tblName", SqlDbType.VarChar, 255),
+					new SqlParameter("@fldName", SqlDbType.VarChar, 255),
+					new SqlParameter("@PageSize", SqlDbType.Int),
+					new SqlParameter("@PageIndex", SqlDbType.Int),
+					new SqlParameter("@IsReCount", SqlDbType.Bit),
+					new SqlParameter("@OrderType", SqlDbType.Bit),
+					new SqlParameter("@strWhere", SqlDbType.VarChar,1000),
+					};
+			parameters[0].Value = "Teacher";
+			parameters[1].Value = "ID";
+			parameters[2].Value = PageSize;
+			parameters[3].Value = PageIndex;
+			parameters[4].Value = 0;
+			parameters[5].Value = 0;
+			parameters[6].Value = strWhere;	
+			return DbHelperSQL.RunProcedure("UP_GetRecordByPage",parameters,"ds");
+		}*/
+
+		#endregion  BasicMethod
+		#region  ExtensionMethod
+
+		#endregion  ExtensionMethod
+	}
+}
+
